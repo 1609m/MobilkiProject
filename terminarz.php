c2427ebe4d8a722acc20c5ef2e42e2051536ff82
Kurkum <34893110+Kurkum@users.noreply.github.com>
2020-08-16 16:26:33 +0200

    terminarz

--- terminarz.php
+++ terminarz.php
@@ -8,12 +8,13 @@
 		header('Location: index.php');
 		exit();
 	}
-	
+
 
 ?>
 <!DOCTYPE html>
 <html>
- <head>
+<head>
+
  <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
@@ -30,73 +31,89 @@
  </head>
  <body>
 	
-  <?php $active = "terminarz"; require "header.php";	?>
-	<main>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-lg-8 my-3 mx-auto bg-secondary text-center text-light">
+    <?php $active = "terminarz"; require "header.php";	?>
+    <script type="text/javascript">
+    
+        function tog() {
+            console.log('!');
+            if($('#aside').hasClass('d-none')) {
+                // $('#main').removeClass('mx-auto');
+                // $('#main').addClass('d-inline-block');
+                $('#aside').removeClass('d-none');
+                $('#aside').addClass('d-inline-block');
+            } else {
+                $('#aside').addClass('d-none');
+                $('#aside').removeClass('d-inline-block');
+            }
+        }
 
-          <h2>Terminarz</h2><br>
-          <a href='terminarz.php?m=0' class='text-warning'>Wróć do dzisiaj</a>
+    </script>
+	    <main>
+		    <div class="container-fluid">
+			    <div class="row">
+				    <div class="col-sm-5 col-md-7 col-lg-8 my-3 mx-auto d-inline-block bg-secondary text-center text-light" id="main">
+
+                        <h2>Terminarz</h2><br>
+                        <a href='terminarz.php?m=0' class='text-warning'>Wróć do dzisiaj</a>
+                        <button onclick="tog()">Click me</button> 
 
 <?php
-  $date = new DateTime();
-  
-  // USTAW MIESIĄC
-  $month = 0;
-  if (isset($_GET['m'])) {
-    $month = $_GET['m'];
-  }
-  if ($month > 0) {
-    for ($i = 0; $i < $month; $i++) {
-      $date->modify('+1 month');
+    $date = new DateTime();
+
+    // USTAW MIESIĄC
+    $month = 0;
+    if (isset($_GET['m'])) {
+        $month = $_GET['m'];
     }
-  } else {
-    for ($i = 0; $i > $month; $i--) {
-      $date->modify('-1 month');
+    if ($month > 0) {
+        for ($i = 0; $i < $month; $i++) {
+            $date->modify('+1 month');
+        }
+    } else {
+        for ($i = 0; $i > $month; $i--) {
+            $date->modify('-1 month');
+        }
     }
-  }
-  
-  $monthName = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
-  
-  // WYSWIETL MIESIAC
-  echo "<form method='post'><h2>
-    <a href='terminarz.php?m=".($month-1)."' class='text-warning'><</a>
-    <b style='display: inline-block; width: 450px;'>".$monthName[$date->format('n') - 1]." ".$date->format('Y')."</b>
-    <a href='terminarz.php?m=".($month+1)."' class='text-warning'>></a></h2></form><br>";
 
-  $offset = ($date->format('N') - $date->format('j')%7 + 7)%7;
+    $monthName = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
 
-  echo "<div class='row mx-auto display-inline-block pl-md-5'>";
-  for($i = 0; $i < 6; $i++) {
-    for($j = 1; $j <= 7; $j++) {
+    // WYSWIETL MIESIAC
+    echo "<form method='post'><h2>
+        <a href='terminarz.php?m=".($month-1)."' class='text-warning'><</a>
+        <b style='display: inline-block; width: 75%;'>".$monthName[$date->format('n') - 1]." ".$date->format('Y')."</b>
+        <a href='terminarz.php?m=".($month+1)."' class='text-warning'>></a></h2></form><br>";
 
-      echo "<div class='col-1 ml-sm-3 m-sm-3 card";
-      $id = $i*7 + $j - $offset;
-      if($id <= $date->format('t') && $id > 0) {
-        if($id == $date->format('j') && $date == new DateTime())
-          echo " bg-white text-dark'";
-        else
-          echo " bg-dark'";
-        echo " id='".$id."'><h4>".$id."</h4></div>";
-      }
-      else
-        echo " bg-secondary'></div>";
+    $offset = ($date->format('N') - $date->format('j')%7 + 7)%7;
 
+    echo "<div class='row mx-auto display-inline-block pl-lg-5'>";
+    for($i = 0; $i < 6; $i++) {
+        for($j = 1; $j <= 7; $j++) {
+
+            echo "<div class='col-md-1 ml-sm-3 m-sm-1 m-lg-3 card";
+            $id = $i*7 + $j - $offset;
+            if($id <= $date->format('t') && $id > 0) {
+                if($id == $date->format('j') && $date == new DateTime())
+                    echo " bg-white text-dark'";
+                else
+                    echo " bg-dark'";
+                echo " id='".$id."'><h4><a href='terminarz.php?m=".$month."&d=".$id."'>".$id."</a></h4></div>";
+            } else
+                echo " bg-secondary'></div>";
+
+        }
+        echo "<div class='w-100'></div>";
     }
-    echo "<div class='w-100'></div>";
-  }
-  
-  
-  
+
+
 ?>
-          </div>
-        </div>
-        
-        <div>
-          <!-- TODO: BOCZNY PANEL -->
-        </div>
-        
+                    </div>
+                </div>
+                
+                <div class="col-6 col-md-3 my-3 mx-auto bg-secondary text-center text-light d-none" id="aside">
+                    <!-- TODO: BOCZNY PANEL -->
+                    tralala
+                </div>
+
 			</div>
 		</div>
 	</main>
@@ -107,5 +124,5 @@
 	
 	<script src="js/bootstrap.min.js"></script>
 	
- </body>
+</body>
 </html>
