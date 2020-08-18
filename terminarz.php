dcc4a891b60066531776c42dc7ff46433d84715b
Kurkum <elrohir0234@gmail.com>
2020-08-18 21:28:58 +0200

    terminarz wyswietlanie testow

--- terminarz.php
+++ terminarz.php
@@ -30,32 +30,16 @@
 
  </head>
  <body>
-	
-    <?php $active = "terminarz"; require "header.php";	?>
-    <script type="text/javascript">
-    
-        function tog() {
-            console.log('!');
-            if($('#aside').hasClass('d-none')) {
-                // $('#main').removeClass('mx-auto');
-                // $('#main').addClass('d-inline-block');
-                $('#aside').removeClass('d-none');
-                $('#aside').addClass('d-inline-block');
-            } else {
-                $('#aside').addClass('d-none');
-                $('#aside').removeClass('d-inline-block');
-            }
-        }
 
-    </script>
+    <?php $active = "terminarz"; require "header.php";	?>
+
 	    <main>
 		    <div class="container-fluid">
 			    <div class="row">
-				    <div class="col-sm-5 col-md-7 col-lg-8 my-3 mx-auto d-inline-block bg-secondary text-center text-light" id="main">
+				    <div class=" col-sm-8 my-3 mx-auto p-4 d-inline-block bg-secondary text-center text-light" id="main">
 
                         <h2>Terminarz</h2><br>
                         <a href='terminarz.php?m=0' class='text-warning'>Wróć do dzisiaj</a>
-                        <button onclick="tog()">Click me</button> 
 
 <?php
     $date = new DateTime();
@@ -79,28 +63,26 @@
 
     // WYSWIETL MIESIAC
     echo "<form method='post'><h2>
-        <a href='terminarz.php?m=".($month-1)."' class='text-warning'><</a>
+		<a href='terminarz.php?m=".($month-1)."' class='text-warning'><</a>
         <b style='display: inline-block; width: 75%;'>".$monthName[$date->format('n') - 1]." ".$date->format('Y')."</b>
         <a href='terminarz.php?m=".($month+1)."' class='text-warning'>></a></h2></form><br>";
 
     $offset = ($date->format('N') - $date->format('j')%7 + 7)%7;
-	
-
-    echo "<div class='row mx-auto display-inline-block pl-lg-5'>";
+    echo "<div class='row mx-md-auto display-inline-block pl-lg-5'>";
     for($i = 0; $i < 6; $i++) {
         for($j = 1; $j <= 7; $j++) {
 
-            echo "<div class='col-md-1 ml-sm-3 m-sm-1 m-lg-3 card";
+            echo "<div class='col-sm-1 ml-md-3 m-sm-1 m-lg-3 card";
             $id = $i*7 + $j - $offset;
             if($id <= $date->format('t') && $id > 0) {
-                if($id == $date->format('j') && $date == new DateTime())
+                if($id == $date->format('j') && $date->format('j.m.Y') == (new DateTime())->format('j.m.Y')) {
                     echo " bg-white text-dark'";
+                }
                 else
                     echo " bg-dark'";
                 echo " id='".$id."'><h4><a href='terminarz.php?m=".$month."&d=".$id."'>".$id."</a></h4></div>";
             } else
                 echo " bg-secondary'></div>";
-
         }
         echo "<div class='w-100'></div>";
     }
@@ -111,13 +93,74 @@
                 </div>
                 
                 <div class="col-6 col-md-3 my-3 mx-auto bg-secondary text-center text-light d-none" id="aside">
-                    <!-- TODO: BOCZNY PANEL -->
-                    tralala
+                    <h3 class="text-right"><a href="terminarz.php<?php echo "?m=".$month; ?>" class='text-warning'>X</a></h3>
+<?php
+
+    $day = $_GET['d'];
+    $month = $date->format('m');
+    $year = $date->format('Y');
+    $sideDate = new datetime("$day.$month.$year");
+    echo "<h4>".$sideDate->format('d.m.Y')."</h4><hr style='height: 5px; background: #373a3d; border: 0px;'>";
+
+    require_once "dbconnect.php";
+    $conn = new mysqli($host, $user, $pass, $db);
+    $sideDateFormat = $sideDate->format('Y.m.d');
+    $result = $conn->query("SELECT * FROM paczkazad WHERE termin='$sideDateFormat'");
+    $i = 0;
+    $napisTesty = "";
+    while($row = $result->fetch_row())
+	{
+		if ($napisTesty == "") {
+            echo "<h4>Testy:</h4>";
+            $napisTesty = "Testy";
+        }
+        $resultKl = $conn->query("SELECT nazwa FROM klasy WHERE id=$row[1]");
+        $nazwaKlasy = ($resultKl->fetch_assoc())['nazwa'];
+        echo "<b><h5 class='text-left'>".++$i.".</h5>Klasa: ".$nazwaKlasy."<br />";
+        
+        $zad = $conn->query("SELECT zadania_id FROM paczkap WHERE paczkazad_id=$row[0] LIMIT 1");
+        $zad = $zad->fetch_row();
+        $przedmiot = $conn->query("SELECT przedmiot_id FROM zadania WHERE id=$zad[0]");
+        $przedmiot = $przedmiot->fetch_row();
+        $przedmiotNazwa = $conn->query("SELECT nazwa FROM przedmioty WHERE id=$przedmiot[0]");
+        $przedmiotNazwa = $przedmiotNazwa->fetch_row();
+        echo "Przedmiot: ".$przedmiotNazwa[0]."</b>";
+	}
+    $conn->close();
+
+?>
+
                 </div>
 
 			</div>
 		</div>
 	</main>
+
+    <script type="text/javascript">
+
+        function tog(aside) {
+            if (aside != "0") {
+                $('#aside').removeClass('d-none');
+                $('#aside').addClass('d-inline-block');
+                $('#main').removeClass('col-sm-8');
+                $('#main').addClass('col-sm-5').addClass('col-md-8');
+                $('.card').addClass('col-md-1');
+                $('.card').removeClass('col-sm-1');
+            } else {
+                console.log('else');
+                $('#aside').addClass('d-none');
+                $('#aside').removeClass('d-inline-block');
+                $('#main').addClass('col-sm-8');
+                $('#main').removeClass('col-sm-5').addClass('col-md-8');
+                $('.card').removeClass('col-md-1');
+                $('.card').addClass('col-sm-1');
+            }
+        }
+
+        let aside = <?php if (isset($_GET['d'])) {echo $_GET['d'];} else {echo "0";} ?>;
+        tog(aside.toString());
+
+    </script>
 	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	
